625e1e2731c2d2294dd7bc2b249916f9dbb08c11
enjayyy <147984667+enjayyy@users.noreply.github.com>
2025-05-01 16:41:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function addTask(){
         listContainer.appendChild(li);
         li.addEventListener("click", function(){
             li.style.textDecoration="line-through";
-            li.classList.toggle("checked"); // remember:checked is a css class that holds the checked img icon
+            li.classList.toggle("checked"); // 
 
         })
         let removeButton = document.createElement("button");
